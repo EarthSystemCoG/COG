85b64ba00d1fcbd48d4b8ffa5df1de43e1d1157c
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2012-09-25 15:50:08 -0600

    Preventing renaming of files un upload.

--- cog/models/doc.py
+++ cog/models/doc.py
@@ -4,10 +4,30 @@ from project import Project
 from django.contrib.auth.models import User
 import os
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
-# function to determine the upload path dynamically at runtime
 def get_upload_path(instance, filename):
+    '''Function to determine the upload path dynamically at runtime.'''
     return os.path.join(getattr(settings, "FILEBROWSER_DIRECTORY"), str(instance.project.short_name.lower()), filename)
+
+class OverridingFileStorage(FileSystemStorage):
+    '''Subclass of FileSystemStorage that overrides existing files (in the same folder).'''
+
+    # This method is actually defined in Storage
+    def save(self, name, content):
+      # must delete current file first  
+      if self.exists(name):
+          print 'Deleting existing file=%s' % name
+          self.delete(name)     
+      # also, look for Doc objects for the same named file  
+      prefix = getattr(settings, "FILEBROWSER_DIRECTORY", "")
+      filepath = name[len(prefix):]
+      docs = Doc.objects.filter(path__endswith=filepath)
+      for doc in docs:
+          doc.delete()
+      return super(OverridingFileStorage, self).save(name, content)
+  
+ofs = OverridingFileStorage()
 
 # A generic document that can be uploaded to the server and attached to a Post
 class Doc(models.Model):
@@ -18,7 +38,7 @@ class Doc(models.Model):
     path = models.CharField(max_length=400, blank=True)
     description = models.TextField(blank=True)
     # upload path is obtained dynamically from the callable function
-    file = models.FileField(upload_to=get_upload_path)
+    file = models.FileField(upload_to=get_upload_path, storage=ofs)
     publication_date = models.DateTimeField('Date Published', auto_now_add=True)
     update_date = models.DateTimeField('Date Updated', auto_now=True)
     project = models.ForeignKey(Project)
