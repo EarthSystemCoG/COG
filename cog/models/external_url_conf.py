05c4ac0ac5b063aca722d03811c15cbd1498ec33
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-08-30 08:18:26 -0600

    Refactoring External URLs.

--- cog/models/external_url_conf.py
+++ cog/models/external_url_conf.py
@@ -10,33 +10,60 @@ class ExternalUrlConf(object):
         self.label = label
         self.type = type
         
+# dictionary of External URL (key, type)
+EXTERNAL_URL_TYPES = {
+                      "BLOG" : "blog",
+                      "REPOSITORY": "repository",
+                      "HOMEPAGE": "homepage",
+                      "REFERENCE": "reference",
+                      "TRACKER": "tracker",
+                      "USECASE": "usecase",
+                      "POLICY": "policy",
+                      "ROADMAP": "roadmap",
+                      "DOWNLOAD": "download",
+                      "ADMIN_GUIDE": "admin_guide",
+                      "USER_GUIDE": "user_guide",
+                      "FAQ": "faq",
+                      "CODE_EXAMPLE": "code_example",
+                      "DEVELOPER_GUIDE": "developer_guide",
+                      "DESIGN_DOCUMENT": "design_document",
+                      "TESTING": "testing",
+                      "CHECKLIST": "checklist",
+                      "MINUTE": "minute",
+                      "METRIC": "metric",
+                      "RELEASE_SCHEDULE": "release_schedule",
+                      }
+        
 # list of pre-defined External URL pages
-EXTERNAL_URL_PAGES = [ ExternalUrlConf('blogs','Blog','blog'),
-                       ExternalUrlConf('repositories','Repositories','repository'),
-                       ExternalUrlConf('','Home Page','homepage'),
-                       ExternalUrlConf('','Reference','reference'),
-                       ExternalUrlConf('trackers','Tracker','tracker'),   
-                       ExternalUrlConf('usecases','Use Case','usecase'),
-                       ExternalUrlConf('policies','Policies','policy'), 
-                       ExternalUrlConf('roadmap','Roadmap','roadmap'), 
-                       ExternalUrlConf('download','Download','download'),
-                       ExternalUrlConf('admin_guide','Administrator\'s Guide','admin_guide'),    
-                       ExternalUrlConf('user_guide','User\'s Guide','user_guide'), 
-                       ExternalUrlConf('faq','FAQ','faq'), 
-                       ExternalUrlConf('code_examples','Code Examples','code_example'),
-                       ExternalUrlConf('developer_guide','Developer\'s Guide','developer_guide'), 
-                       ExternalUrlConf('design_docs','Design Documents','design_document'),
-                       ExternalUrlConf('testing','Testing','testing'),
-                       ExternalUrlConf('checklist','Checklist','checkclist'), 
-                       ExternalUrlConf('minutes','Minutes','minute'),
-                       ExternalUrlConf('metrics','Metrics','metric'),
-                       ExternalUrlConf('release_schedules','Release Schedules','release_schedule'),
+EXTERNAL_URL_PAGES = [ ExternalUrlConf('blogs', 'Blog', EXTERNAL_URL_TYPES["BLOG"]),
+                       ExternalUrlConf('repositories', 'Repositories', EXTERNAL_URL_TYPES["REPOSITORY"]),
+                       ExternalUrlConf('', 'Home Page', EXTERNAL_URL_TYPES["HOMEPAGE"]),
+                       ExternalUrlConf('', 'Reference', EXTERNAL_URL_TYPES["REFERENCE"]),
+                       ExternalUrlConf('trackers', 'Tracker', EXTERNAL_URL_TYPES["TRACKER"]),   
+                       ExternalUrlConf('usecases', 'Use Case', EXTERNAL_URL_TYPES["USECASE"]),
+                       ExternalUrlConf('policies',' Policies', EXTERNAL_URL_TYPES["POLICY"]), 
+                       ExternalUrlConf('roadmap', 'Roadmap', EXTERNAL_URL_TYPES["ROADMAP"]), 
+                       ExternalUrlConf('download', 'Download', EXTERNAL_URL_TYPES["DOWNLOAD"]),
+                       ExternalUrlConf('admin_guide', 'Administrator\'s Guide', EXTERNAL_URL_TYPES["ADMIN_GUIDE"]),    
+                       ExternalUrlConf('user_guide', 'User\'s Guide', EXTERNAL_URL_TYPES["USER_GUIDE"]), 
+                       ExternalUrlConf('faq', 'FAQ', EXTERNAL_URL_TYPES["FAQ"]), 
+                       ExternalUrlConf('code_examples', 'Code Examples', EXTERNAL_URL_TYPES["CODE_EXAMPLE"]),
+                       ExternalUrlConf('developer_guide', 'Developer\'s Guide', EXTERNAL_URL_TYPES["DEVELOPER_GUIDE"]), 
+                       ExternalUrlConf('design_docs', 'Design Documents', EXTERNAL_URL_TYPES["DESIGN_DOCUMENT"]),
+                       ExternalUrlConf('testing', 'Testing', EXTERNAL_URL_TYPES["TESTING"]),
+                       ExternalUrlConf('checklist', 'Checklist', EXTERNAL_URL_TYPES["CHECKLIST"]), 
+                       ExternalUrlConf('minutes', 'Minutes', EXTERNAL_URL_TYPES["MINUTE"]),
+                       ExternalUrlConf('metrics', 'Metrics', EXTERNAL_URL_TYPES["METRIC"]),
+                       ExternalUrlConf('release_schedules', 'Release Schedules', EXTERNAL_URL_TYPES["RELEASE_SCHEDULE"]),
 ]
 
 # map frm suburl -> ExternalUrlConf instance
-EXTERNAL_URL_PAGE_MAP = {}
+EXTERNAL_URL_SUBURL_MAP = {}
+# map from type -> ExternalUrlConf instance
+EXTERNAL_URL_TYPE_MAP = {}
 for obj in EXTERNAL_URL_PAGES:
-    EXTERNAL_URL_PAGE_MAP[obj.suburl] = obj
+    EXTERNAL_URL_SUBURL_MAP[obj.suburl] = obj
+    EXTERNAL_URL_TYPE_MAP[obj.suburl] = obj
 
 def external_url_choices():
     '''Provides valid options when building ExternalUrl choices.'''
