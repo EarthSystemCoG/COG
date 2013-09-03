fcfebaa23d406fa5cdd49c72119b3b893e08105e
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-09-03 08:21:43 -0600

    Removing unused applications 'cog' and 'remap'.

--- cog/context_processors.py
+++ cog/context_processors.py
@@ -1,7 +1,8 @@
 # Django contect processor to add COG-specific settings to the request context,
-# and make them available inside the template files
+# and make them available inside the template files.
 from django.conf import settings # import the settings file
 
 def cog_settings(request):
-    # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'FACEBOOK_APPLICATION_ID': settings.FACEBOOK_APPLICATION_ID }
+    
+    # example key-value pair
+    return {'DEFAULT_SEARCH_URL': settings.DEFAULT_SEARCH_URL }
--- remap/remap_test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def remap(srcgrid, dstgrid, weights, method=""):
-    command = 'cat %s %s > %s' % (srcgrid, dstgrid, weights)
-    print 'command= %s ' % command
-    status = os.system(command)
-    return status
