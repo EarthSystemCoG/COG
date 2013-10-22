0ccc180a3e43c5c585a6b359acc110aee89b8a01
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-10-22 11:00:56 -0600

    Inserting script for updating search configuration.

---
+++ configure_search.py
@@ -0,0 +1,16 @@
+# Python script to configure off-band a project advanced search
+import sys, os, ConfigParser
+
+sys.path.append( os.path.abspath(os.path.dirname('.')) )
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from cog.config.search import config_project_search
+#from django.conf import settings
+
+print 'Upgrading COG'
+
+# read search configurations
+configs = { 'NCPP': 'cog/config/search/ncpp.cfg' }
+
+for key in configs:
+    config_project_search(key, configs[key])
