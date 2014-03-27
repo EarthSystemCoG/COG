f65503d57d4c09a02f55bd940fc4b94acee8ac42
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-03-27 15:58:37 -0700

    Working on roles.

--- cog/models/organizational_role.py
+++ cog/models/organizational_role.py
@@ -7,7 +7,7 @@ from cog.models.constants import ROLE_CATEGORY_LEAD, ROLE_CATEGORY_MEMBER, ORGAN
 
 class OrganizationalRole(models.Model):
 
-    title = models.CharField(max_length=200, blank=False, verbose_name='Title',  help_text='String used to succinctly describe an organizational role.')
+    title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Title',  help_text='String used to succinctly describe an organizational role.')
     description = models.TextField(blank=True, null=True, verbose_name='Description', help_text='Long description providing extra information about an organizational role.')
     type = models.CharField(max_length=50, blank=False, null=False, choices=ORGANIZATIONAL_ROLE_CV, verbose_name='Type', help_text='Type of organizational role (choose from controlled vocabulary).')
     category = models.CharField(max_length=50, blank=True, null=False, default='Member', choices=ORGANIZATIONAL_ROLE_CATEGORIES_CV, verbose_name='Category', help_text='Lead or Member role.')
