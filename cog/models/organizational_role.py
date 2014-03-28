22cded35d0b623712db49ab865dbbcbb9b0a6216
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-03-28 07:19:04 -0600

    Removing first empty option in roles.

--- cog/models/organizational_role.py
+++ cog/models/organizational_role.py
@@ -7,9 +7,9 @@ from cog.models.constants import ROLE_CATEGORY_LEAD, ROLE_CATEGORY_MEMBER, ORGAN
 
 class OrganizationalRole(models.Model):
 
-    title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Title',  help_text='String used to succinctly describe an organizational role.')
+    type = models.CharField(max_length=50, blank=False, null=False, default='', choices=ORGANIZATIONAL_ROLE_CV, verbose_name='Type', help_text='Type of organizational role (choose from controlled vocabulary).')
+    title = models.CharField(max_length=200, blank=True, null=True, verbose_name='Title',  help_text='Optional string used to succinctly describe an organizational role.')
     description = models.TextField(blank=True, null=True, verbose_name='Description', help_text='Long description providing extra information about an organizational role.')
-    type = models.CharField(max_length=50, blank=False, null=False, choices=ORGANIZATIONAL_ROLE_CV, verbose_name='Type', help_text='Type of organizational role (choose from controlled vocabulary).')
     category = models.CharField(max_length=50, blank=True, null=False, default='Member', choices=ORGANIZATIONAL_ROLE_CATEGORIES_CV, verbose_name='Category', help_text='Lead or Member role.')
     project = models.ForeignKey(Project)
 
