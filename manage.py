0629aa759407e005c0e7e1f9e314e530f3ef828a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-06-25 08:07:51 -0600

    Reverting back to version compatible with django 1.6.5

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import django
 if __name__ == "__main__":
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    django.setup()
+    #django.setup()
     
 
     from django.core.management import execute_from_command_line
