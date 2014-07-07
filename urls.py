a341855ae8a5eb0b97f47a18d801443e8103c200
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-07-07 05:50:05 -0600

    Working on URLs.

--- urls.py
+++ urls.py
@@ -35,8 +35,8 @@ urlpatterns = patterns('',
     # other media (when NOT served through the Apache web server)
     # Note: the media must be located under <application>/static/<application>, not static/<application>
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT} ),
-    #url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT} ),
-    #url(r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT} ),    
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT} ),
+    url(r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT} ),    
 
 
 )
