364a61265d6221a6387da2dbc8d71513df647891
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-07-10 14:17:55 -0600

    Upgrading scripts, instructions.

--- scripts/cog_backup.sh
+++ scripts/cog_backup.sh
@@ -5,4 +5,5 @@
 # cat ~/.pgpass
 # host_name:port:database_name:database_user:database_password
 
+export PATH=/usr/local/pgsql/bin:$PATH
 pg_dump -p 5433 -U cogdbadmin cogdb > /tmp/cogdb_$(date +%Y-%m-%d).sql
