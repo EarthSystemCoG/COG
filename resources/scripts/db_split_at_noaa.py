6ecc3e6f95e65eae09f8d33da2aad7a4611a7a85
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-09-05 04:11:04 -0600

    Working on dtaabase setup at NOAA.

---
+++ resources/scripts/db_split_at_noaa.py
@@ -0,0 +1,40 @@
+'''
+Script to perform database setup at CoG-NOAA after 2.6.1 deployment (CoG sites split).
+
+'''
+import os
+import sys
+import cog
+import shutil
+path = os.path.dirname(cog.__file__)
+sys.path.append( path )
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.core.exceptions import ObjectDoesNotExist
+from cog.models import UserProfile
+
+from cog.models import Project
+
+
+# load CU, NOAA Sites
+
+# before DNS change
+#cuSite = Site.objects.get(domain='cog-cu.colorado.edu')
+#noaaSite = Site.objects.get(domain='www.earthsystemcog.org')
+
+# after DNS change:
+cuSite = Site.objects.get(domain='www.earthsystemcog.org')
+noaaSite = Site.objects.get(domain='cog-esgf.esrl.noaa.gov')
+if cuSite != Site.objects.get_current():
+    raise Exception("Running script for wrong site, exiting")
+
+dryrun = True
+
+# loop over projects
+noaaProjects = ['HIWPP', 'CMDTF', 'CoupledNEMS', 
+                       'HIWPP_HurricaneNest', 'HIWPP_Hydrostatic', 'HIWPP_Internal',
+                       'HIWPP_Management', 'HIWPP_NMME', 'HIWPP_NonHydrostatic', 'HIWPP_TestProgram',
+                       ]
