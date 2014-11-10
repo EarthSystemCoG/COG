76ad63284a0da2db4e2a67f191e2b83406994289
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-11-10 13:25:16 -0700

    Inserting notification after registration.

--- cog/views/views_access_control.py
+++ cog/views/views_access_control.py
@@ -6,17 +6,18 @@ Module containing views for managing access control groups.
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseForbidden, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from sqlalchemy.orm.exc import NoResultFound
 
-from cog.models import User
+from cog.models import User, UserProfile, getSiteAdministrators
 from cog.services.registration import esgfRegistrationServiceImpl as registrationService
 from cog.plugins.esgf.objects import ROLE_USER, ROLE_PUBLISHER, ROLE_SUPERUSER, ROLE_ADMIN
 from cog.forms import PermissionForm
 from django.contrib.sites.models import Site
 from cog.models import getPeerSites
 from cog.utils import getJson
+from django.core.urlresolvers import reverse
 from collections import OrderedDict
 
 from cog.notification import notify
@@ -45,7 +46,8 @@ def ac_subscribe(request, group_name):
         approved = registrationService.subscribe(request.user.profile.openid(), group_name, ROLE_USER)
         
         # notify site administrators
-        # TODO
+        if not approved:
+            notifyAdmins(group_name, request.user.id, request)
         
         # return feedback to user
         return render_to_response(template, 
@@ -98,7 +100,6 @@ def ac_process(request, group_name, user_id):
             for role in [ROLE_USER, ROLE_PUBLISHER, ROLE_SUPERUSER, ROLE_ADMIN]:
                 # retrieve approve status from POST data and store it in ESGF database
                 approve = form.cleaned_data.get('%sPermissionCheckbox' % role, False) # only True values are transmitted in POST data
-                print 'processing role=%s approve=%s' % (role, approve)
                 try:
                     registrationService.process(openid, group_name, role, approve)
                 
@@ -106,6 +107,11 @@ def ac_process(request, group_name, user_id):
                     if approve: # create new permission, but only if approve=True
                         registrationService.subscribe(openid, group_name, role)
                         registrationService.process(openid, group_name, role, approve)
+                        
+            # notify user
+            permissions = registrationService.list(user.profile.openid(), group_name)
+            notifyUser(group_name, request.user, permissions)
+                
 
             # (GET-POST-REDIRECT)
             return HttpResponseRedirect( reverse('ac_process', kwargs={'user_id': user.id, 'group_name': group_name })
@@ -148,3 +154,41 @@ def ac_list(request):
                               {'groups': _groups, 'title': 'ESGF Data Access Control Groups' }, 
                               context_instance=RequestContext(request))
     
+def notifyAdmins(group_name, user_id, incomingRequest):
+    
+    user = get_object_or_404(User, pk=user_id)
+
+    subject = "'%s' Data Access Request" % group_name
+    message = "User '%s' has requested membership in group '%s'" % (user.get_full_name(), group_name)
+    message += '\nPlease process the request at: %s' % incomingRequest.build_absolute_uri( reverse('ac_process', kwargs={ 'group_name':group_name, 'user_id':user.id }) )
+
+    # user attributes
+    message +="\n"
+    message += "\nFirst Name: %s" % user.first_name
+    message += "\nLast Name: %s" % user.last_name
+    message += "\nUser Name: %s" % user.username
+    message += "\nEmail: %s" % user.email
+    
+    # openid
+    message += "\nOpenID is: %s" % user.profile.localOpenid()
+
+    # user profile attributes
+    profile = UserProfile.objects.get(user=user)
+    message += "\nInstitution: %s" % profile.institution
+    message += "\nDepartment: %s" % profile.department
+    message += "\nCity: %s" % profile.city
+    message += "\nState: %s" % profile.state
+    message += "\nCountry: %s" % profile.country
+
+    for admin in getSiteAdministrators():
+        notify(admin, subject, message)
+        
+def notifyUser(group_name, user, permissions):
+    
+    subject = "'%s' Data Access Notification" % group_name
+    message = "Your permissions in group '%s' have been updated" % group_name
+    
+    for (role, status) in permissions.items():
+        message += "\nRole: %s status=%s" % (role, status)
+        
+    notify(user, subject, message)
