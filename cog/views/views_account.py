e9f0a748b5544e7ac4510c5174dc23de7ef45f9c
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-04-07 04:06:37 -0600

    Working on login redirection.

--- cog/views/views_account.py
+++ cog/views/views_account.py
@@ -261,7 +261,7 @@ def user_add(request):
                 login_url += ("&next=%s" % _next)
                 # redirect to absolute URL (possibly at an another site)
                 if 'http' in _next:
-                    url = urlparse('_next')
+                    url = urlparse(_next)
                     login_url = url.scheme + '//' + url.netloc + "/" + login_url
             
             response = HttpResponseRedirect(login_url)
