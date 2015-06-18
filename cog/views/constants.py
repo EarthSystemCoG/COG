8f43e29045a8916c5a328f33e860a72f4c7ab101
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-06-18 16:00:53 -0600

    Sanitizing the value of 'filter_by' and 'order_by'.

--- cog/views/constants.py
+++ cog/views/constants.py
@@ -4,3 +4,6 @@ LOCAL_PROJECTS_ONLY_MESSAGE = "This operation is limited to local projects only"
 GLOBUS_NOT_ENABLED_MESSAGE = "This CoG site is not registered with Globus"
 BAD_REQUEST = "Bad Request"
 SAVED = "saved"
+
+VALID_ORDER_BY_VALUES = ['title', '-title', 'update_date', '-update_date']
+VALID_FILTER_BY_VALUES = ['all','image','text','presentation','program']
