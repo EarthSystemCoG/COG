63e1e54c887048e05592a35bbbe03f3834d92673
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-01-04 13:54:13 -0700

    Removing django-pagination from list of project activity.

--- cog/views/views_signal.py
+++ cog/views/views_signal.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from cog.models import Project
+from cog.views.utils import paginate
 
 def signal_list(request, project_short_name):
     
@@ -12,5 +13,5 @@ def signal_list(request, project_short_name):
     return render_to_response('cog/signal/signal_list.html', 
                               {'project': project, 
                                'title': '%s Activity' % project.full_name(),
-                               'project_signals' : project.signals() },
+                               'project_signals' : paginate(project.signals(), request, max_counts_per_page=10) },
                                context_instance=RequestContext(request))
