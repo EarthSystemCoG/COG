aeb44661491ad54bce2223fc7737f7db22ba9e61
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-01-13 13:41:48 -0700

    Adding instruction on chmode.

--- resources/scripts/db_backup.sh
+++ resources/scripts/db_backup.sh
@@ -4,6 +4,9 @@
 # NOTE: to run as cron, the script must access the postgres user password from file ~/.pgpass
 # cat ~/.pgpass
 # host_name:port:database_name:database_user:database_password
+#
+# also permissions must be set as follows:
+# chmod 400 ~/.pgpass
 
 export PATH=/usr/local/pgsql/bin:$PATH
 pg_dump -p 5432 -U dbsuper cogdb > /tmp/cogdb_$(date +%Y-%m-%d).sql
