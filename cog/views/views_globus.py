325d27f7fc015751dab10a711f7bfcc85fba233f
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-04-18 17:37:16 -0600

    Fixing esg-cog installation script.

--- cog/views/views_globus.py
+++ cog/views/views_globus.py
@@ -262,10 +262,10 @@ def submit(request):
 	for source_endpoint, source_files in download_map.items():
 		
 		# submit transfer request
+		# FIXME
 		_source_files = []
 		for sf in source_files:
 			_source_files.append( sf.replace('/esg_dataroot', ''))
-		print 'SOURCE_FILE=%s' % _source_files
 		openid = request.user.profile.openid()
 		task_id = submitTransfer(openid, username, access_token, source_endpoint, _source_files, target_endpoint, target_folder)
 		task_ids.append(task_id)
