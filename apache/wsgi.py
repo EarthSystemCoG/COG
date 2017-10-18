0af4af28512b4fa9a96193144e458fc068dfc196
Luca Cinquini <cinquini@LMC-038261.local>
2017-10-18 13:35:59 -0600

    Using env variable SSL_CERT_FILE

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -23,6 +23,7 @@ sys.path.insert(0, '/usr/local/cog/cog_install')
 os.environ["COG_CONFIG_DIR"] = "/usr/local/cog/cog_config"
 os.environ['HTTPS'] = "on" # instructs Django to prepend 'https' to fully generated links
 os.environ["SSL_CERT_DIR"] = "/etc/grid-security/certificates"
+os.environ["SSL_CERT_FILE"] = "/etc/certs/esgf-ca-bundle.crt"
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
@@ -32,6 +33,7 @@ print 'Using Python path: %s' % sys.path
 print 'PYTHONPATH=%s' % os.environ.get('PYTHONPATH', None)
 print 'LD_LIBRARY_PATH=%s' % os.environ.get('LD_LIBRARY_PATH', None)
 print 'SSL_CERT_DIR=%s' % os.environ.get('SSL_CERT_DIR', None)
+print 'SSL_CERT_FILE=%s' % os.environ.get('SSL_CERT_FILE', None)
 
 try:
     application = get_wsgi_application()
