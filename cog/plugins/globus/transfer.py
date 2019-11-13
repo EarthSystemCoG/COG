ed5da0f70a4971e8e87d94fbe55c99326af8f7a8
mauzey1 <mauzey1@llnl.gov>
2019-11-12 17:43:08 -0800

    Update Globus downloads to use Globus SDK

--- cog/plugins/globus/transfer.py
+++ cog/plugins/globus/transfer.py
@@ -7,10 +7,7 @@ Module to interact with Globus data transfer services.
 from datetime import datetime, timedelta
 from cog.site_manager import siteManager
 if siteManager.isGlobusEnabled():    
-    from globusonline.transfer.api_client import Transfer
-    from globusonline.transfer.api_client import TransferAPIClient
-    from globusonline.transfer.api_client import TransferAPIError
-    from globusonline.transfer.api_client import x509_proxy
+    from globus_sdk import TransferData
 import os
 import urlparse
 
@@ -30,66 +27,56 @@ def generateGlobusDownloadScript(download_map):
     return script
 
 
-def activateEndpoint(api_client, endpoint, myproxy_server=None, username=None, password=None, cert=None, key=None):
-    if (not myproxy_server or not password) and (not myproxy_server or not cert):
+def activateEndpoint(transfer_client, endpoint, myproxy_server=None, username=None, password=None):
+    if not myproxy_server or not password:
         # Try to autoactivate the endpoint
-        code, reason, result = api_client.endpoint_autoactivate(endpoint, if_expires_in=2880)
-        print "Endpoint Activation: %s. %s: %s" % (endpoint, result["code"], result["message"])
+        result = transfer_client.endpoint_autoactivate(endpoint, if_expires_in=2880)
+        print("Endpoint Activation: %s. %s: %s" % (endpoint, result["code"], result["message"]))
         if result["code"] == "AutoActivationFailed":
             return (False, "")
         return (True, "")
 
-    code, reason, reqs = api_client.endpoint_activation_requirements(endpoint)
+    requirements = transfer_client.endpoint_get_activation_requirements(endpoint)
+    requirements_json = requirements.data
 
-    # Activate the endpoint using an X.509 user credential stored by esgf-idp in /tmp/x509up_<idp_hostname>_<username>
-    if cert and key:
-        cred_file = "/tmp/x509up_%s_%s" % (myproxy_server, username)
-        with open(cred_file, 'w') as cred:
-            cred.write(cert)
-            cred.write(key)
-        public_key = reqs.get_requirement_value("delegate_proxy", "public_key")
-        try:
-            proxy = x509_proxy.create_proxy_from_file(cred_file, public_key, lifetime_hours=72)
-        except Exception as e:
-            print "Could not activate the endpoint: %s. Error: %s" % (endpoint, str(e))
-            return False
-        reqs.set_requirement_value("delegate_proxy", "proxy_chain", proxy)
-    else:
-        # Activate the endpoint using MyProxy server method
-        reqs.set_requirement_value("myproxy", "hostname", myproxy_server)
-        reqs.set_requirement_value("myproxy", "username", username)
-        reqs.set_requirement_value("myproxy", "passphrase", password)
-        reqs.set_requirement_value("myproxy", "lifetime_in_hours", "168")
+    # Activate the endpoint using MyProxy server method
+    for i, d in enumerate(requirements_json["DATA"]):
+        if d["type"] == "myproxy":
+            if d["name"] == "hostname":
+                requirements_json["DATA"][i]["value"] = myproxy_server
+            elif d["name"] == "username":
+                requirements_json["DATA"][i]["value"] = username
+            elif d["name"] == "passphrase":
+                requirements_json["DATA"][i]["value"] = password
+            elif d["name"] == "lifetime_in_hours":
+                requirements_json["DATA"][i]["value"] = "168"
 
     try:
-        code, reason, result = api_client.endpoint_activate(endpoint, reqs)
+        result = transfer_client.endpoint_activate(endpoint, requirements_json)
     except Exception as e:
-        print "Could not activate the endpoint: %s. Error: %s" % (endpoint, str(e))
+        print("Could not activate the endpoint: %s. Error: %s" % (endpoint, str(e)))
         return (False, str(e))
-    if code != 200:
-        print "Could not aactivate the endpoint: %s. Error: %s - %s" % (endpoint, result["code"], result["message"])
+    if result["code"] != "Activated.MyProxyCredential":
+        print("Could not aactivate the endpoint: %s. Error: %s - %s" % (endpoint, result["code"], result["message"]))
         return (False, result["message"])
 
-    print "Endpoint Activation: %s. %s: %s" % (endpoint, result["code"], result["message"])
+    print("Endpoint Activation: %s. %s: %s" % (endpoint, result["code"], result["message"]))
 
     return (True, "")
 
 
-def submitTransfer(api_client, source_endpoint, source_files, target_endpoint, target_directory):
+def submitTransfer(transfer_client, source_endpoint, source_files, target_endpoint, target_directory):
     '''
     Method to submit a data transfer request to Globus.
     '''
-    
-    # obtain a submission id from Globus
-    code, message, data = api_client.transfer_submission_id()
-    submission_id = data["value"]
-    print "Obtained transfer submission id: %s" % submission_id
     
     # maximum time for completing the transfer
     deadline = datetime.utcnow() + timedelta(days=10)
     
     # create a transfer request
-    transfer_task = Transfer(submission_id, source_endpoint, target_endpoint, deadline)
+    transfer_task = TransferData(transfer_client, source_endpoint, target_endpoint, deadline=deadline)
+    print("Obtained transfer submission id: %s" % transfer_task["submission_id"])
+
     for source_file in source_files:
         source_directory, filename = os.path.split(source_file)
         target_file = os.path.join(target_directory, filename) 
@@ -97,11 +84,11 @@ def submitTransfer(api_client, source_endpoint, source_files, target_endpoint, t
     
     # submit the transfer request
     try:
-        code, reason, data = api_client.transfer(transfer_task)
+        data = transfer_client.submit_transfer(transfer_task)
         task_id = data["task_id"]
-        print "Submitted transfer task with id: %s" % task_id
+        print("Submitted transfer task with id: %s" % task_id)
     except Exception as e:
-        print "Could not submit the transfer. Error: %s" % str(e)
+        print("Could not submit the transfer. Error: %s" % str(e))
         task_id = "Could not submit the transfer. Please contact the ESGF node admin to investigate the issue."
     
     return task_id
